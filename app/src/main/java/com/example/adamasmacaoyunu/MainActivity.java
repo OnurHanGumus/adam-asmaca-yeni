3c6e240afafb0f67fcaec8ef77a5a11115dec1dd
OnurHanGumus <onurhangumus@outlook.com>
2022-04-08 15:02:19 +0300

    -Resimler eklendi, -yanlış seçim olunca kayberttin mesajının çıkması sağlandı, -yanlış seçimde fotoğraflar arası geçiş sağlandı. -artık kelimeyi (ikinci edittext'e) yazınca doğrudan kabul ediyor.

--- app/src/main/java/com/example/adamasmacaoyunu/MainActivity.java
+++ app/src/main/java/com/example/adamasmacaoyunu/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.adamasmacaoyunu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -13,13 +16,20 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextInputEditText tahminTxt;
+    TextInputEditText harfTahminTxt;
+    TextInputEditText kelimeTahminTxt;
     TextView kelimeTxt;
+    ImageView adamImg;
 
     String deger;
     //String gosterilecekDeger = "";
     StringBuilder gosterilecekDeger;
     char[] karakterDizisi;
+
+    boolean harfeSahipMi = false;
+    int maxHataToleransi = 6;
+    int mevcutHata = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +50,58 @@ public class MainActivity extends AppCompatActivity {
 
     void initComponents(){
 
-        tahminTxt = findViewById(R.id.tahminTxt);
+        harfTahminTxt = findViewById(R.id.harfTahminTxt);
         kelimeTxt = findViewById(R.id.kelimeTxt);
+        kelimeTahminTxt = findViewById(R.id.kelimeTahminTxt);
+        adamImg = findViewById(R.id.adamImg);
+
+
     }
 
     void registerEventHandlers() {
-        tahminTxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
 
-                sahipMi(tahminTxt.getText().toString());
+        harfTahminTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count == 0){
+                    return;
+                }
+                harfeSahipMi(harfTahminTxt.getText().toString());
+                kazanildiMi();
                 kelimeTxt.setText(gosterilecekDeger);
             }
-        }
-        );
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+
+        kelimeTahminTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                girilenKelimeDogruMu();
+                kelimeTxt.setText(gosterilecekDeger);
+
+
+            }
+        });
     }
     public void degerUret(){
         Random random = new Random();
@@ -86,14 +134,80 @@ public class MainActivity extends AppCompatActivity {
         kelimeTxt.setText(gosterilecekDeger);
     }
 
-    public void sahipMi(String gelen){
+    public void harfeSahipMi(String gelen){
         Log.d("sahipMi",gelen);
         for (int i = 0; i < deger.length(); i++) {
             if (gelen.equals(Character.toString(karakterDizisi[i]))){
                 gosterilecekDeger = gosterilecekDeger.replace(i*2, i*2+1, Character.toString(karakterDizisi[i]));
+                harfeSahipMi = true;
             }
         }
         Log.w("deneme", gosterilecekDeger.toString());
+        if (harfeSahipMi == false){
+            yanlisHarf();
+        }
+        harfeSahipMi = false;
+    }
+    public void girilenKelimeDogruMu(){
+        if (deger.equalsIgnoreCase(kelimeTahminTxt.getText().toString())){
+            for (int i = 0; i < deger.length(); i++) {
+                gosterilecekDeger = gosterilecekDeger.replace(i*2, i*2+1, Character.toString(karakterDizisi[i]));
+            }
+            kazandildi();
+        }
+
+    }
+    public void yanlisHarf(){
+        mevcutHata++;
+        if (mevcutHata == 0){
+            adamImg.setImageResource(R.drawable.adam0);
+
+        }
+        else if(mevcutHata == 1){
+            adamImg.setImageResource(R.drawable.adam1);
+
+        }
+        else if(mevcutHata == 2){
+            adamImg.setImageResource(R.drawable.adam2);
+
+        }
+        else if(mevcutHata == 3){
+            adamImg.setImageResource(R.drawable.adam3);
+
+        }
+        else if(mevcutHata == 4){
+            adamImg.setImageResource(R.drawable.adam4);
+
+        }
+        else if(mevcutHata == 5){
+            adamImg.setImageResource(R.drawable.adam5);
+
+        }
+        else if(mevcutHata == 6){
+            adamImg.setImageResource(R.drawable.adam6);
+
+        }
+
+        if (mevcutHata == maxHataToleransi){
+            kaybedildi();
+        }
+    }
+
+    public void kaybedildi(){
+        Toast.makeText(this, "Kaybettiniz.", Toast.LENGTH_SHORT).show();
+        this.setTitle("Kaybettiniz");
+    }
+    public void kazanildiMi(){
+
+        int altcizgiKalmisMi = gosterilecekDeger.indexOf("_");
+        if (altcizgiKalmisMi == -1){
+            kazandildi();
+        }
+
+    }
+    public void kazandildi(){
+        Toast.makeText(this, "Kazandınız.", Toast.LENGTH_SHORT).show();
+        this.setTitle("Kazandınız");
     }
 
     public void BasligiDegistir(){
